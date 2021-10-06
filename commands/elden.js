285004a5b001a0ebeae55973112a0cf75e18051f
Rasmus Kivinen <rasmus.kivinen@tuni.fi>
2021-10-06 13:34:56 +0300

    Fix math error in elden.js

--- commands/elden.js
+++ commands/elden.js
@@ -12,10 +12,10 @@ module.exports = {
         const er = mjs("21012022", "DDMMYYYY")
         let dur = mjs.duration(er.diff(today))
         
-        const days = Math.round(dur.asDays())
+        const days = Math.floor(dur.asDays())
         dur.subtract(mjs.duration(days, "days"))
 
-        const hours = Math.floor(dur.asHours())
+        const hours = Math.round(dur.asHours())
 
         reply = String(days) + " days and " + String(hours) + " hours until Elden Ring!"
 		await interaction.reply(reply);
